a520f70e6344afaaa7a36f2cb95c4056653d3713
rahul-10-byte <kalyankarrahul500@gmail.com>
2021-10-15 23:50:39 +0530

    Fix Bugs

---
+++ src/layouts/Default.layout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+//Components
+import Navbar from "../components/Navbar/navbar.component";
+import HeroCarousal from "../components/HeroCarousel/HeroCarousel.components";
+const DefaultLayout = (props) => {
+  return (
+    <>
+      <Navbar />
+      <HeroCarousal />
+      {props.children}
+    </>
+  );
+};
+
+export default DefaultLayout;
---
+++ src/layouts/Movie.layout.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useContext } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+// Components
+import MovieNavbar from "../components/Navbar/movieNavbar.component";
+//Context
+import { MovieContext } from "../context/movie.context";
+
+const MovieLayout = (props) => {
+  const { id } = useParams();
+  const { movie, setMovie } = useContext(MovieContext);
+
+  useEffect(() => {
+    const requestMovie = async () => {
+      const getMovieData = await axios.get(`/movie/${id}`);
+      setMovie(getMovieData.data);
+    };
+    requestMovie();
+  }, [id]);
+
+  return (
+    <>
+      <MovieNavbar />
+      {props.children}
+    </>
+  );
+};
+export default MovieLayout;
